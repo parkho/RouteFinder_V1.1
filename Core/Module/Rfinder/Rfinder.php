480db23021813a4b9aeb63d5db38489acf5a62a4
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-11-27 20:15:10 +0330

    Version Check Is Needed

--- Core/Module/Rfinder/Rfinder.php
+++ Core/Module/Rfinder/Rfinder.php
@@ -5,9 +5,16 @@ class Rfinder extends CodonModule
 
 	public function index() 
 	   {
-            
+            $revision = trim(file_get_contents(CORE_PATH.'/version'));
+			if($revision != 'simpilot 5.5.2')
+				{
+					echo '<center>phpVMS Version Installed Is Not Compatible With This Module!</center><br />';
+					echo '<center>phpVMS Version Installed: '.$revision.'</center>';
+				}
+			else
+			{
             $this->show('/rfind/Rfinder.php');
-			
+			}
             
         }
 }
